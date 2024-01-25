7983503e480d3f8a2f1e49f883acc09afbdfe9ce
Kamil Młodziński <haniakkamil@gmail.com>
2024-01-25 11:16:24 +0100

    Updates

---
+++ precache-manifest.4aada85b3f146428bcb7a789f57959cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca278b3c9b96499bab72f611dbd93a5",
+    "url": "/converter-react/index.html"
+  },
+  {
+    "revision": "38c95b665c1b17350397",
+    "url": "/converter-react/static/css/main.c674c78c.chunk.css"
+  },
+  {
+    "revision": "540ed6de48c689f61dd1",
+    "url": "/converter-react/static/js/2.6b4a1066.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/converter-react/static/js/2.6b4a1066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "484995143528a92f506a",
+    "url": "/converter-react/static/js/3.ddf649c5.chunk.js"
+  },
+  {
+    "revision": "38c95b665c1b17350397",
+    "url": "/converter-react/static/js/main.1d5b1fa7.chunk.js"
+  },
+  {
+    "revision": "d89f2a2c94e14f41350e",
+    "url": "/converter-react/static/js/runtime-main.b2c4cc67.js"
+  },
+  {
+    "revision": "a059ddb6b3caebb37b9513ed8ddbe013",
+    "url": "/converter-react/static/media/background.a059ddb6.png"
+  }
+]);
